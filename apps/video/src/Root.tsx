84fc46d7077e9c9c6859c79b0ddd0fc61cd59d6b
antonin36330 <antonin.marcon@gmail.com>
2026-04-27 19:37:06 +0200

    feat(video): register LaunchAnnouncement composition in Root

--- apps/video/src/Root.tsx
+++ apps/video/src/Root.tsx
@@ -1,5 +1,9 @@
 import { Composition } from "remotion";
 import { QuoteCard, quoteCardSchema } from "./compositions/QuoteCard";
+import {
+  LaunchAnnouncement,
+  launchAnnouncementSchema,
+} from "./compositions/LaunchAnnouncement";
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -20,6 +24,17 @@ export const RemotionRoot: React.FC = () => {
             "Quatre jours à Zarautz avec Marceau. Pas un défaut. On revient l'été prochain.",
           vanId: "marceau" as const,
         }}
+      />
+
+      <Composition
+        id="LaunchAnnouncement"
+        component={LaunchAnnouncement}
+        durationInFrames={510}
+        fps={30}
+        width={1080}
+        height={1920}
+        schema={launchAnnouncementSchema}
+        defaultProps={{ siteUrl: "rb-capso.fr" }}
       />
     </>
   );
